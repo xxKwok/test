7426ea70fee98999a90e2aca7fa3669724f7fb3f
歌浅 <162082836+xxKwok@users.noreply.github.com>
2024-12-03 08:12:56 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function colorizeText() {
+  let inputText = document.getElementById('inputText').value;
+  let outputText = '';
+  for (let i = 0; i < inputText.length; i++) {
+    // 生成随机十六进制颜色代码
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    outputText += `<color=${randomColor}">${inputText[i]}</color>`;
+  }
+  document.getElementById('textOutput').innerHTML = outputText;
+}
